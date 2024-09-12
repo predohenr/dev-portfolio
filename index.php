8861b9b9e102d93c299a98e9ed9234afa091848d
predohenr <lopes.pedro@ufca.edu.br>
2024-09-12 16:51:52 -0300

    refactoring landing page to php

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <?php include 'pages/head.php'; ?>
+  </head>
+
+  <body>
+    <!-- Navbar -->
+    <?php include 'pages/navbar.php'; ?>
+    
+    <!-- Hero Section -->
+    <?php include 'pages/hero.php'; ?>
+
+    <!-- More about -->
+    <?php include 'pages/about.php'; ?>
+
+    <!-- Skills section -->
+    <?php include 'pages/skills.php'; ?>
+
+    <!-- Projects section -->
+    <?php include 'pages/projects.php'; ?>
+
+    <!-- Contact section -->
+    <?php include 'pages/contact.php'; ?>
+
+    <!-- Scroll to top -->
+    <i class="scroll-up" id="scroll-up"
+    ><img
+      src="assets/icons/icons8-upward-arrow.gif"
+      class="socicon up-arrow"
+      alt="scroll-up"
+    /></i>
+
+    <!-- Footer section -->
+    <?php include 'pages/footer.php'; ?>
+
+    <!-- Website scripts -->
+    <script src="assets/js/app.js"></script>
+
+    <!-- Ion icons scripts -->
+    <script
+      type="module"
+      src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"
+    ></script>
+    <script
+      nomodule
+      src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"
+    ></script>
+  </body>
+</html>
---
+++ pages/hero.php
@@ -0,0 +1,28 @@
+<section class="hero">
+    <div class="bio-photo">
+    <img
+        src="assets/images/pedro-photo.jpg"
+        alt="Foto Pedro"
+        loading="lazy"
+        class="hero-img"
+    />
+    </div>
+    <div class="bio animate__animated animate__shakeX">
+    <h1 class="bio-title">PEDRO LOPES</h1>
+    <h2 class="bio-subtitle">FULL-STACK DEVELOPER;<br> SOFTWARE ENGINEER;<br> M.SC at <a href="https://cin.ufpe.br">CIn-UFPE</a>.</h2>
+    <p class="bio-text">
+        Cearense morando em Recife-PE.<br>Tenho 22 anos e amo aprender coisas novas!
+    </p>
+    <span class="bio-socials">
+        <a href="https://github.com/predohenr" target="_blank">
+        <img class="socicon" src="assets/icons/github.png">
+        </a>
+        <a href="http://lattes.cnpq.br/9302354041102246" target="_blank">
+        <img class="socicon" src="assets/icons/lattes.png">
+        </a>
+        <a href="https://www.linkedin.com/in/phpedrohenr" target="_blank">
+        <img class="socicon" src="assets/icons/linkedin.png">
+        </a>
+    </span>
+    </div>
+</section>
