c00a32b4cc740c69d2bc12f5dc17ff8a3cbc534f
predohenr <lopes.pedro@ufca.edu.br>
2024-09-13 21:25:45 -0300

    adding academic section and fixing css on smaller devices

--- pages/about.php
+++ pages/about.php
@@ -2,11 +2,10 @@
     <h2>SOBRE MIM</h2>
     <div class="about-content">
         <p>
-        Olá, me chamo Pedro! Sou Bacharel em Ciência da Computação pela UFCA, Desenvolvedor Full-Stack e 
-        atualmente Mestrando em Ciência da Computação pelo CIn-UFPE, na área de Engenharia de Software. 
-        Minha pesquisa gira em torno de fazer uma Análise Comparativa de Fluxos de Trabalho com o Git e 
-        seus impactos na produtividade, qualidade final do produto, buscando entender em quais cenários cada 
-        fluxo pode se sair melhor dependendo do cenário.<br><br>
+        Olá, me chamo Pedro! Sou Desenvolvedor Full-Stack e atualmente Mestrando em Ciência da Computação no 
+        CIn-UFPE, na área de Engenharia de Software. Minha pesquisa gira em torno de fazer uma Análise Comparativa 
+        de Fluxos de Trabalho com o Git e seus impactos na produtividade, qualidade final do produto, buscando 
+        entender em quais cenários cada fluxo pode se sair melhor dependendo do cenário.<br><br>
         Como desenvolvedor, já utilizei HTML, CSS, JS, Bootstrap, Git, Github, Figma, php, C, Python e Java!
         </p>
         <img class="about-img" src="assets/images/pedro-photo2.jpg"
